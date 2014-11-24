60fec8151c8ad1ba6bd6d4f8ccb30d5d2d2f8454
Jose Miguel Melo <melo199435@gmail.com>
2014-11-24 21:07:35 +0000

    comment reformulated

--- config/init.php
+++ config/init.php
@@ -1,8 +1,13 @@
 <?php
-	$BASE_DIR = $_SERVER['DOCUMENT_ROOT'];
+	$BASE_DIR = '/usr/users2/mieic2012/ei12050/public_html';
 	/* NO SAMBA DA FEUP, SUBSTITUIR POR ESTA STRING:
 	 
 	 		/usr/users2/mieic2012/ei12050/public_html
+
+	 	SENAO
+				
+			$_SERVER['DOCUMENT_ROOT']
+
 
 	 */
 ?>
