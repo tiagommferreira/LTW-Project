7808ea4d137eebef03a1891addb40fc56defe19c
Jose Miguel Melo <melo199435@gmail.com>
2014-11-24 22:12:36 +0000

    jquery bugs removed

--- config/init.php
+++ config/init.php
@@ -2,6 +2,8 @@
 	$BASE_DIR = '/usr/users2/mieic2012/ei12050/public_html';
 
 	//$BASE_DIR = $_SERVER['DOCUMENT_ROOT'];
+
+	
 	/* NO SAMBA DA FEUP, SUBSTITUIR POR ESTA STRING:
 	 
 	 		/usr/users2/mieic2012/ei12050/public_html
--- scripts/register_validation.js
+++ scripts/register_validation.js
@@ -1,10 +1,15 @@
 $(function(){
+	// base url pc
+	//var BASE_URL = "http://ltw.feup:8888/projetoLTW";
+	// base url da feup
+	var BASE_URL = "http://gnomo.fe.up.pt/~ei12050/projetoLTW"
+
 // while you are typing using keypress event
 	$("input[name='username']").bind('keyup',function(){
 		// get the value of the input
 		var username = $(this).val();
 		var input = $(this);
-		$.getJSON('http://ltw.feup:8888/projetoLTW/api/users.php?username='+username, function( data ) {
+		$.getJSON(BASE_URL+'/api/users.php?username='+username, function( data ) {
 			if(data.username == username){
 				input.css("border-bottom", "2px solid red");
 			}
@@ -17,7 +22,7 @@ $(function(){
 		if(!username){
 			input.removeAttr( 'style' );
 		}else{
-			$.getJSON('http://ltw.feup:8888/projetoLTW/api/users.php?username='+username, function( data ) {
+			$.getJSON(BASE_URL+'/api/users.php?username='+username, function( data ) {
 				if(data.username != username){
 					input.css("border-bottom", "2px solid green");
 				}
