15df2535d727642bb986d16f124981d1982b7634
Tiago Ferreira <tiagommferreira55@gmail.com>
2014-11-27 11:25:32 +0000

    login and register on index now changes cursor

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
 
 function listeners() {
+	$('.register-title').hover(function() {
+        $(this).css('cursor','pointer');
+    });
 	$(".register-title").click(function(){
   		$(".login").hide();
   		$(".register").show();
 	});
 
+	$('.login-title').hover(function() {
+        $(this).css('cursor','pointer');
+    });
 	$(".login-title").click(function(){
   		$(".register").hide();
   		$(".login").show();
