c1adf5f6b4c099d6fbf2056229d6a01004edbe1c
Jose Miguel Melo <melo199435@gmail.com>
2014-12-03 17:36:40 +0000

    about us text changed; about us css improved

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <script src="scripts/script.js"></script>
 <script src="scripts/register_validation.js"></script>
 <script src="scripts/login_validation.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
   
   <?php session_start(); ?>
 <body>
@@ -34,19 +35,37 @@
   <h1 id="about_us_button" class="about-us-title">About us</h1>
     <div id="about_us">
     <p align=justify>
-      Hey, welcome to our website! <br>
-      We are basically a group of 3 classmates, that made this site a class that we atend at FEUP. <br>
-      That class is named LTW, short for "Linguagens e Tecnologias Web".<br>
-      The main purpose of this site is to provida a user based interface, for creating and voting on the polls that a user made and submited.<br>
-      In the main page, the user can see some stats. <br>
-      Basically how many polls you made, and how many public polls he hasn't answered yet.<br>
-      When creating a poll, the user can add as many response option he wants.<br>
-      He can even set it public or private, and add a photo.<br>
-      Pretty sweet, ahm? <br>
-      You can even assemble a list of some of the polls you made and send a link to your friends, share on Facebook or Twitter. <br>
-      We hope you have fun using our website, as much as we had making it <br>
-      <br>
-      José, José and Tiago.<br>
+      <h2>Hey, welcome to our website!</h2> <br>
+     
+      The main purpose of this website is to provid a user based interface for voting on polls created by users.<br><br>
+      <b>A poll can:</b>
+        <ul style="list-style-type: none;">
+          <li > 
+            <center><i class="fa fa-plus fa-3x" style="color: #84C242;"></i><br>
+            Have as many responses as the user wants</center><br><br><br><br>
+          </li> 
+          <li> 
+            <center><i class="fa fa-lock fa-3x" style="color: #FFB833;"></i><br>
+            Be public or private</center><br><br><br><br>
+          </li>
+          <li> 
+            <center><i class="fa fa-picture-o fa-3x" style="color: #33A0FF;"></i><br>
+            Add an image to the poll</center><br><br><br><br>
+          </li>
+          <li>
+            <center><i class="fa fa-facebook fa-3x" style="color: #3B5998; margin-right: 4%;"></i>
+            <i class="fa fa-twitter fa-3x" style="color: #00aced; margin-right: 4%;"></i>
+            <i class="fa fa-envelope-o fa-3x" style="color: #4297C2;"></i><br>
+             Be shared with others</center><br><br><br><br>
+          </li>
+        </ul>
+      Pretty sweet, ahm? But it's <b>not just that</b>!!!<br><br>
+      You can also assemble a <b style="color: #3B5998;">list of some polls</b>  you created and <b style="color: #3B5998;">send a link </b>to your friends and <b style="color: #3B5998;">share on Facebook or Twitter</b>. <br><br>
+      
+     <h4 style="color: #84C242;"> We hope you have fun and take advantages of using our website!!</h4>
+     <br><br><b>
+      Our team</b>,<br>
+      José Melo, José Cardoso and Tiago Ferreira
 	</p>
 
 
